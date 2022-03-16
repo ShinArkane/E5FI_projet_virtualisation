e26cb7cfd9bfdf5ac6846c9cae9d83d32080887f
ShinArkane <47025999+ShinArkane@users.noreply.github.com>
2022-03-16 14:21:22 +0100

    initCommit

--- src/main/java/com/project/registry/RegistryApplication.java
+++ src/main/java/com/project/registry/RegistryApplication.java
@@ -1,4 +1,4 @@
-package com.projet.Registry;
+package com.project.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/project/registry/RegistryApplicationTests.java
+++ src/test/java/com/project/registry/RegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.projet.Registry;
+package com.project.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
